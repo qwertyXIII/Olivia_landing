4f6062007f1e040ad970ec9fe06947d7153cc0c9
Dmitriy Vertinskiy <dimosok137@gmail.com>
2023-11-01 15:21:17 +0700

    Temporary disconnection from DB

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ practicesButtons.forEach(el => {
   })
 });
 
+/*
 formSubmit.addEventListener('click', (e) => {
   e.preventDefault();
   if (formValidator(form, ['popup-input-training', 'popup-input-name', 'popup-input-number', 'popup-input-email'])) {
@@ -38,10 +39,6 @@ formSubmit.addEventListener('click', (e) => {
   }
 });
 
-particlesJS.load('particles-js', 'utils/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
-
 buttons.forEach((e) => {
   e.addEventListener('click', (e) => {
     serverDataSender('user_actions', {userAgent: navigator.userAgent, action: 'click', object: e.target.getAttribute('observer')});
@@ -54,6 +51,11 @@ links.forEach((e) => {
 })
 
 serverDataSender('visit', {userAgent: navigator.userAgent, location: Intl.DateTimeFormat().resolvedOptions().timeZone});
+*/
+
+particlesJS.load('particles-js', 'utils/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
 
 serverDataLoader('review')
   .then((answer) => {
@@ -66,3 +68,45 @@ serverDataLoader('review')
 setTimeout(() => {
   createNotification('ok','Записывайтесь ко мне на тренинги!')
 }, 30000);
+
+
+
+/// временное решение ===>>>
+formSubmit.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (formValidator(form, ['popup-input-training', 'popup-input-name', 'popup-input-number', 'popup-input-email'])) {
+    formLoader.classList.add('popup__loading_active');
+    formSubmit.classList.add('popup__send_inactive');
+    formSubmit.disabled = true;
+
+
+    
+    const BOT_TOKEN = '6424649838:AAGGfAMevuRHqZJ1JQ5-B0qAMb_WQ46XIIQ';
+    const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+    const url = `${TELEGRAM_API}?chat_id=812132855&text=${encodeURIComponent(createMSG({name: formInputName.value, typeOfService: formInputTraining.value, number: formInputNumber.value, telegram: formInputTelegram.value, email: formInputEmail.value, consentToMailing: formInputCheckbox.checked}))}`;
+    fetch(url, { muteHttpExceptions: true })
+    .then(() => {
+      createNotification('ok', `Успешно отправленно! Ожидайте, я связжусь с вами в ближайшее время`);
+      formLoader.classList.remove('popup__loading_active');
+      formSubmit.classList.remove('popup__send_inactive');
+      formSubmit.disabled = false;
+      popup.classList.add('popup_closed')
+    }).catch(() => {
+      createNotification('error', `Что-то пошло не так. Попробуйте позже.`);
+      formLoader.classList.remove('popup__loading_active');
+      formSubmit.classList.remove('popup__send_inactive');
+      formSubmit.disabled = false;
+    });
+  }
+});
+
+function createMSG(data) {
+  return `
+  Новая заявка 🙌
+  
+  🙋‍♀️ Имя: ${data.name}
+  📞 Телефон: ${data.number}
+  ✈️ Телеграмм: ${data.telegram}
+  📃 Тип заявки: ${data.typeOfService}
+  🤝 Согласие рассылку: ${data.consentToMailing}`
+}
